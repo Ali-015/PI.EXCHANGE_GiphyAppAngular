a9bf68bd0ebcfea380ad8338c5a8dfc876a25627
Ali015 <alisadiq015@gmail.com>
2024-01-31 14:24:40 +0530

    Adding proper folder structure

--- src/app/services/gif-service.service.ts
+++ src/app/services/gif-service.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
-import { environment } from '../environment/environment';
+import { environment } from '../../environment/environment';
 import { SearchResponse } from '../interfaces/gifs.interfaces';
 
 @Injectable()
